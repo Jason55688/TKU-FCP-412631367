99a898e2bd76bb9a5d2a38dfdbb05bc40f538a04
Jason55688 <jason0911423885@gmail.com>
2024-07-03 11:39:10 +0800

    Add files via upload

---
+++ ch2_6.java
@@ -0,0 +1,13 @@
+public class ch2_6 {    //定義類別 ch2_3
+    public static void main(String[] args) { 
+        int n1 = 12 ;
+        int n2 = 26 ;  //宣告變數a和b
+
+        System.out.println(n1+"+"+n2+"="+(n1+n2));
+        System.out.println("n1+n2="+(n1+n2));
+        System.out.println(n1+n2);  //印出運算結果
+
+    }
+
+    
+}
