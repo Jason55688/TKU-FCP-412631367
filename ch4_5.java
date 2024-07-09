c2d41d414ef98e89cd1902ca56c4c4bcd91af121
Jason55688 <jason0911423885@gmail.com>
2024-07-09 11:33:02 +0800

    0709

---
+++ ch4_5.java
@@ -0,0 +1,9 @@
+public class ch4_5 {
+    public static void main(String[] args) {
+        boolean a= true, b=false ;
+        System.out.printf("%b || %b = %b\n",a,b, a||b); //邏輯運算子OR
+        System.out.printf("%b && %b = %b\n",a,b, a&&b); //邏輯運算子AND
+        System.out.printf("!%b == %b\n",a,!a);  //邏輯運算子NOT
+    }
+    
+}
